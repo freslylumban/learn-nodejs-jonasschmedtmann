4dfae5a4d3c5125b4c363d10c60c08b280b81d4c
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-02-27 08:56:06 +0700

    [Fresly] - commit from folder 2 video 6. Blocking and Non-Blocking Asynchronous Nature of Node.js and repeat learning from video 1

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 
+// Blocking code execution
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
+
+// // Non blocking code execution
+// fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+//   console.log(data);
+// });
+// console.log('Reading file...');
 
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreate on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
