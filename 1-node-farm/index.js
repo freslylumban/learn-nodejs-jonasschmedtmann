69b87f0e668d220445319d609de38e8e713759c8
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-02-27 09:28:21 +0700

    [Fresly] - commit from folder 2 video 7. Reading and Writing Files Asynchronously

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 
-// Blocking code execution
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// Blocking code execution --> Synchronous Way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreate on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written, Fresly!');
 
 // // Non blocking code execution
 // fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
@@ -10,6 +13,25 @@ console.log(textIn);
 // });
 // console.log('Reading file...');
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreate on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written, Fresly!');
+
+// Non-blocking code --> Asynchronous Way
+// ES6 syntax
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('ERROR!');
+
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+        console.log('Your file has been written, Fresly!')
+      });
+    });
+  });
+});
+console.log('Will read file!');
+
+// Nb:
+// ES5 syntax
+// function(err, data1) {}
