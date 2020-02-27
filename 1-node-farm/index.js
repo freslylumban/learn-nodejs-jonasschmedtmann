7d81dfdc26d9031c5393de212cbe58cd67929d88
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-02-27 11:20:38 +0700

    [Fresly] - commit from folder 2 video 10. Building a (Very) Simple API - async

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -53,6 +53,14 @@ const server = http.createServer((req, res) => {
     res.end('This is the OVERVIEW');
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT');
+  } else if (pathName === '/api') {
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      const productData = JSON.parse(data);
+      res.writeHead(200, {
+        'Context-type': 'application/json'
+      });
+      res.end(data);
+    })
   } else {
     res.writeHead(404, {
       'Context-type': 'text/html',
