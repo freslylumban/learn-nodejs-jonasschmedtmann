7d26914347782339a99af76a475040c42024a6ba
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-02-27 15:05:56 +0700

    [Fresly] - commit from folder 2 video 13. Parsing Variables from URLs

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -66,10 +66,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // OVERVIEW page
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
       'Context-type': 'text/html'
     })
@@ -78,11 +78,16 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
   // PRODUCT page
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT');
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Context-type': 'text/html'
+    })
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
   // API
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, {
       'Context-type': 'application/json'
     });
