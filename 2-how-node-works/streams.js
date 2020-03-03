a4dfb21a4d0ae1bb5b699e9312267aa7dccde28a
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-03 10:50:52 +0700

    [Fresly] - commit from folder 4 video 9. Streams in Practice - Solution 2

--- 2-how-node-works/streams.js
+++ 2-how-node-works/streams.js
@@ -3,9 +3,23 @@ const server = require('http').createServer();
 
 server.on('request', (req, res) => {
   // Solution 1
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  // fs.readFile("test-file.txt", (err, data) => {
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+
+  // Solution 2: Streams
+  const readable = fs.createReadStream("test-file.txt");
+  readable.on("data", chunk => {
+    res.write(chunk);
+  });
+  readable.on("end", () => {
+    res.end();
+  });
+  readable.on("error", err => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end("File not found");
   });
 });
 
