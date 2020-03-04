7d4d9d788be454e21e37ffa99afe71527ace02c9
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-04 11:40:53 +0700

    [Fresly] - commit from folder 5 video 4. Building Promises

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -1,20 +1,36 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then(res => {
-      console.log(res.body);
-
-      fs.writeFile('dog-img.txt', res.body.message, err => {
-        if (err) return console.log(err);
-        console.log('Random dog image saved to file!');
-      });
+const readFileProm = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file!')
+      resolve(data);
     })
-    .catch(err => {
-      console.log(err.message);
-    });
-});
+  })
+}
+
+const writeFileProm = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject('I could not write file!');
+      resolve('Success!');
+    })
+  })
+}
+
+readFileProm(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then(res => {
+    console.log(res.body);
+    return writeFileProm('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
