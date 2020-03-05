a7195ba1babbd9a87484bc66d85598c9fa819aff
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-05 14:25:25 +0700

    [Fresly] - commit from folder 6 video 14. Using 3rd-Party Middleware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1. MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -16,6 +21,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2. ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -107,6 +114,8 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3. ROUTES
+
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -117,6 +126,8 @@ app
   .get(getTourById)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4. START SERVER
 
 const port = 8000;
 app.listen(port, () => {
