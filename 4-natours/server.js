c8281ced9dee9d91a0fd277d8cb063c0253aae98
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-10 13:11:25 +0700

    [Fresly] - commit from folder 8 video 4. Creating a Simple Tour Model

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -16,10 +16,27 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false
   })
-  .then(con => {
-    console.log(con.connections);
+  .then(() => {
+    // console.log(con.connections);
     console.log('DB connections successful!');
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price']
+  }
+});
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
