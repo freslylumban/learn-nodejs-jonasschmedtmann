dea3de1c7d8cc836658a6cb740de19f4c5c75b7e
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-11 14:07:42 +0700

    [Fresly] - commit from folder 8 video 11. Deleting Documents

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -81,11 +81,20 @@ const updateTour = async (req, res) => {
   }
 };
 
-const deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 module.exports = {
