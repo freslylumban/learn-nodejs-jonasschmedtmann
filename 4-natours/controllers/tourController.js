358b515d5e32206f0272400c99241ea8d9d3c05b
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-12 09:31:56 +0700

    [Fresly] - commit from folder 8 video 14. Making the API Better Filtering

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -2,14 +2,17 @@ const Tour = require('./../models/tourModel');
 
 const getAllTours = async (req, res) => {
   try {
+    // BUILD QUERY
     const queryObj = {...req.query};
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach(el => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    const query = Tour.find(queryObj);
 
-    const tours = await Tour.find(queryObj);
+    // EXECUTE QUERY
+    const tours = await query;
 
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
