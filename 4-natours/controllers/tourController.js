08e74b99a096b0dc02074d6d0b78f28dce2e63c9
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-12 09:15:39 +0700

    [Fresly] - commit from folder 8 video 14. Making the API Better Filtering - req query mongodb

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -2,7 +2,20 @@ const Tour = require('./../models/tourModel');
 
 const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    console.log(req.query);
+
+    // const tours = await Tour.find();
+
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy'
+    // });
+
+    const tours = await Tour.find()
+      .where('duration')
+      .equals(5)
+      .where('difficulty')
+      .equals('easy');
 
     res.status(200).json({
       status: 'success',
