d80f990fcdb8fbccdaf687560429b02c798281ac
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-16 23:16:28 +0700

    [Fresly] - commit from folder 9 video 12. Handling Mongoose Validation Errors

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -27,7 +27,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a difficulty'],
       enum: {
         values: ['easy', 'medium', 'difficult'],
-        message: 'Diffuculty is either easy, medium, or difficult'
+        message: 'Difficulty is either easy, medium, or difficult'
       }
     },
     ratingsAverage: {
