8da83d6931382526ddc73f3da9d6a22da9cdd12c
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-03-16 21:35:43 +0700

    [Fresly] - commit from folder 9 video 9. Errors During Development vs Production

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,12 +1,44 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    // responseCode: err.statusCode,
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // OPERATIONAL, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      // responseCode: err.statusCode,
+      status: err.status,
+      message: err.message
+    });
+
+    // PROGRAMMING or OTHER unknown error: don't leak error details
+  } else {
+    // 1. Log Error
+    console.log('ERROR', err);
+
+    // 2. Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    // responseCode: err.statusCode,
-    status: err.status,
-    message: err.message
-  });
-}
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
+};
