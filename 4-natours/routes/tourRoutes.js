3afbd7e34e46d93857fe8598127c5088c8e8fff8
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-05 22:51:35 +0700

    [Fresly] - commit from folder 11 video 12. Implementing Simple Nested Routes

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -10,6 +10,7 @@ const {
   getMonthlyPlan
 } = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
 
 const router = express.Router();
 
@@ -30,6 +31,22 @@ router
   .route('/:id')
   .get(getTourById)
   .patch(updateTour)
-  .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), deleteTour);
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    deleteTour
+  );
+
+// POST /tour/:tourId/reviews
+// GET /tour/:tourId/reviews
+// GET /tour/:tourId/reviews/:reviewId
+
+router
+  .route('/:tourId/reviews')
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
 
 module.exports = router;
