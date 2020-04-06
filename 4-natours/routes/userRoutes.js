30152358d51a892e2cad641a3cec65592938d217
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-06 16:26:03 +0700

    [Fresly] - commit from folder 11 video 18. Adding a me Endpoint

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -5,16 +5,28 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+
 router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
+
 router.patch('/resetPassword/:token', authController.resetPassword);
+
 router.patch(
   '/updateMyPassword',
   authController.protect,
   authController.updatePassword
 );
+
+router.get(
+  '/me',
+  authController.protect,
+  userController.getMe,
+  userController.getUserById
+);
+
 router.patch('/updateMe', authController.protect, userController.updateMe);
+
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
