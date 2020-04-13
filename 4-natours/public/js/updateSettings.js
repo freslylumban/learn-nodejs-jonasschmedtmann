dabc23c1cd3c88aed3d1bb24095cdfe89aef27a6
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-14 01:34:30 +0700

    [Fresly] - commit from folder 12 video 24. Updating User Password with Our API

--- 4-natours/public/js/updateSettings.js
+++ 4-natours/public/js/updateSettings.js
@@ -2,21 +2,20 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateData = async (name, email) => {
+// type is either to 'password' or 'data'
+export const updateSettings = async (data, type) => {
   try {
+    const url = type === 'password' ? 'updateMyPassword' : 'updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:8000/api/v1/users/updateMe',
-      data: {
-        name: name,
-        email: email
-      }
+      url: `http://127.0.0.1:8000/api/v1/users/${url}`,
+      data: data
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
-
   } catch (err) {
     showAlert('error', err.response.data.message);
   }
