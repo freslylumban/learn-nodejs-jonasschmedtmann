2a3570794b4343b451428211fced1ff47e1b55e1
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-15 13:27:03 +0700

    [Fresly] - commit from folder 13 video 5. Resizing Images - edited .cache

--- 4-natours/public/js/bundle.js
+++ 4-natours/public/js/bundle.js
@@ -8980,7 +8980,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43083" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39603" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
