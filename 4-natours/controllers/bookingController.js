bf0d8b62749f1f7659c8fdce74546b9e9b5537ef
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-22 00:13:05 +0700

    [Fresly] - commit from folder 14 video 10. Finishing Payments with Stripe Webhooks - edited 2

--- 4-natours/controllers/bookingController.js
+++ 4-natours/controllers/bookingController.js
@@ -66,7 +66,7 @@ exports.webhookCheckout = (req, res, next) => {
       process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (err) {
-    return res.status(400).send('Webhook error: ${err.message}');
+    return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
   if (event.type === 'checkout.session.completed') {
